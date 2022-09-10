5bf77a8d30d191e5e04ee78e981224036aae9ed9
Anzhela Bolshakova <32403959+apbolshakova@users.noreply.github.com>
2022-09-10 03:31:59 +0300

    Create 43 - Exclude.ts

---
+++ 43 - Exclude.ts
@@ -0,0 +1,15 @@
+/*
+  43 - Exclude
+  -------
+  Implement the built-in Exclude<T, U>
+  
+  > Exclude from T those types that are assignable to U
+  
+  For example:
+  
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+*/
+
+type MyExclude<T, U> = T extends U ? never : T;
